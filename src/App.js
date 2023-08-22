9b250fe8644ae99f44007b40a8deb7b3c75b43f4
Serjura <85463530+Serjura@users.noreply.github.com>
2023-08-22 11:21:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       formData.append('file', selectedFile);
 
       try {
-        const response = await fetch('http://max-human-pose-estimator/model/predict', {
+        const response = await fetch('http://max-human-pose-estimator.demo.svc.cluster.local/model/predict', {
           method: 'POST',
           body: formData,
         });
